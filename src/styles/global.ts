1a25ab5633adf836f91a30b5133d10ff1f604736
Andre Teles <andre.telestp@gmail.com>
2020-09-11 11:25:05 -0300

    chore: setup environment

---
+++ src/styles/global.ts
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  *{
+    margin:0;
+    padding: 0;
+    outline:0;
+    box-sizing: border-box;
+  }
+
+  html{
+    font-size: 62.5%;
+  }
+
+  body{
+    background: #312e38;
+    color: #FFF;
+    text-rendering: optimizeLegibility ;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  body,input,button{
+    font: 1.6rem 'Roboto Slab', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  }
+
+  button{
+    cursor: pointer;
+  }
+  h1,h2,h3,h4,h5,h6,strong{
+    font-weight: 500;
+  }
+
+`;
