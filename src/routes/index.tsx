cc253d432ab7fa6113889523e738aef04bb04655
Andre Teles <andre.telestp@gmail.com>
2020-09-19 15:24:47 -0300

    chore: add support to private routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 import SignIn from '../pages/Signin';
 import SignUp from '../pages/Signup';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" exact component={SignUp} />
+    <Route path="/dashboard" exact component={Dashboard} isPrivate />
   </Switch>
 );
 
