06e1b53c69dfa494bf0524c971dd4615b0d4daa1
Andre Teles <andre.telestp@gmail.com>
2020-11-09 10:37:10 -0300

    chore: add signin test

---
+++ src/__tests__/pages/Signin.spec.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Signin from '../../pages/Signin';
+
+jest.mock('react-router-dom', () => {
+  return {
+    useHistory: jest.fn(),
+    Link: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
+describe('Signin Page', () => {
+  it('should be able to sign in', () => {
+    render(<Signin />);
+
+    // const emailField = getByPlaceholderText('E-mail');
+    // const passwordField = getByPlaceholderText('Senha');
+
+    // fireEvent;
+  });
+});
